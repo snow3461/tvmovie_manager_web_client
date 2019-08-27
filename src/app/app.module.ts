b91603e2a2c9df29577bf4fdeeb1f96ea231d42c
Thomas Jarry <thomas.jarry@yahoo.fr>
2019-08-27 23:37:27 +0200

    Removed unneeded components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { AppComponent } from "./app.component";
+import { NavMenuComponent } from "./nav-menu/nav-menu.component";
+import { MovieComponent } from "./components/movie/movie.component";
+import { MoviesViewComponent } from "./components/movies-view/movies-view.component";
+import { MoviesService } from "./services/movies.service";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    MovieComponent,
+    MoviesViewComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: "", component: MoviesViewComponent, pathMatch: "full" }
     ])
   ],
-  providers: [],
+  providers: [MoviesService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
